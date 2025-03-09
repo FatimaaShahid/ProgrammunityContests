17acf5cbb9362ce22181f02bd9a7963cb21cfa24
Fatima Shahid <160149262+FatimaaShahid@users.noreply.github.com>
2025-03-09 14:28:53 +0500

    Removed all files

--- 3Idiots.py
+++
@@ -1,7 +0,0 @@
-t=int(input())
-count = 0
-for _ in range(t):
-    l = int(input()).split()
-    if l.count(1)>1:
-        count += 1
-print(count)
--- Syllables.py
+++
@@ -1,5 +0,0 @@
-l = int(input()).split()
-
-if l.count(5)==2 and l.count(7)==1:
-    print("YES")
-print("NO")
